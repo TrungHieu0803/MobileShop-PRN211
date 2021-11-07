38c524b0b563680784e797bf0a2960e276276226
THANHLTHE150044 <thanhlthe150044@fpt.edu.vn>
2021-11-07 17:36:47 +0700

    admin index

---
+++ MobileShop/Controllers/AdminController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MobileShop.Models;
+using System.Linq;
+using PagedList;
+using System.Web.Mvc;
+
+
+namespace MobileShop.Controllers
+{
+    public class AdminController : Controller
+    {
+        MobileShopContext context = new MobileShopContext();
+        public ActionResult Index(int? page)
+        {
+            if (page == null) page = 1;
+            var sanpham = context.Sanphams.OrderBy(x => x.Masp);
+            int pageSize = 8;
+            int pageNumber = (page ?? 1);
+            return View(sanpham.ToPagedList(pageNumber, pageSize));
+        }
+        
+    }
+}
