cf5fd914c8d0ecfb2a03f2fcffbfb6cc3224732e
phanducmanhfpt <manhpdhe151290@fpt.edu.vn>
2021-11-08 01:12:55 +0700

    update SP

---
+++ MobileShop/Controllers/DanhmucController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MobileShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MobileShop.Controllers
+{
+    public class DanhmucController : Controller
+    {
+        MobileShopContext db = new MobileShopContext();
+        public IActionResult Index()
+        {
+           var danhmuc = db.Hangsanxuats.ToList();
+            return PartialView(danhmuc);
+        }
+    }
+}
