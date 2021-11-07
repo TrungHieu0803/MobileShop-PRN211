515ad14f9b240a8a1ec60aef543accbbd3d733c0
THANHLTHE150044 <thanhlthe150044@fpt.edu.vn>
2021-11-07 23:36:44 +0700

    layout admin sửa

--- MobileShop/Controllers/AdminController.cs
+++ MobileShop/Controllers/AdminController.cs
@@ -3,7 +3,7 @@ using MobileShop.Models;
 using System.Linq;
 using PagedList;
 using System.Web.Mvc;
-
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace MobileShop.Controllers
 {
@@ -16,8 +16,28 @@ namespace MobileShop.Controllers
             var sanpham = context.Sanphams.OrderBy(x => x.Masp);
             int pageSize = 8;
             int pageNumber = (page ?? 1);
+            ViewBag.Hangsanxuats = context.Hangsanxuats.ToList();
             return View(sanpham.ToPagedList(pageNumber, pageSize));
         }
-        
+        public ActionResult AddProduct()
+        {
+            var hangselected = new SelectList(context.Hangsanxuats, "Mahang", "Tenhang");
+            ViewBag.Mahang = hangselected;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddProduct(Sanpham sanpham)
+        {
+            try
+            {
+                context.Sanphams.Add(sanpham);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
     }
 }
